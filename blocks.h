89ad8b4bab884d7f40dfec58a110e0913bb76813
Apig Sharbo <apigsharbo@gmail.com>
2021-04-10 12:12:19 +0400

    Change update interval to 1s, just in case components update externally in another way

--- blocks.h
+++ blocks.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "~/.local/bin/statusbar/brightness-script",			0,		1},
-	{"", "~/.local/bin/statusbar/volume",				0,		2},
-	{"", "date '+%Y %b %d (%a) %r'",				    1,		3},
+    /*Icon*/    /*Command*/     /*Update Interval*/ /*Update Signal*/
+    {" ", "~/.local/bin/statusbar/brightness-script",   1,      1},
+    {"", "~/.local/bin/statusbar/volume",               1,      2},
+    {"", "date '+%Y %b %d (%a) %r'",                    1,      3},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
